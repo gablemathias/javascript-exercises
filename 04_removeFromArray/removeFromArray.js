ac4d7f117493bcfdc14aa89a9f6dbc23ad6e8f43
Gabriel Mathias <gablemathias@gmail.com>
2022-02-21 22:07:20 -0300

    ex. from 01 to 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+function spliceArray(array, itemRemoval){
+  let counter = array.length;
+  while (counter--){
+    if (array[counter] === itemRemoval){
+      array.splice(array.indexOf(itemRemoval),1);
+    }
+  }
+}
 
+const removeFromArray = function(array, ...removeTargetItem) {
+  let targetArray = array;
+  for(let i = 0; i <= removeTargetItem.length; i++){
+    targetRemoval = removeTargetItem[i]
+    spliceArray(targetArray, targetRemoval);
+  }
+  return targetArray;
 };
 
 // Do not edit below this line
